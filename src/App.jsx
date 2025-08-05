4a43901dd80ba8dae2cfdc10a66f256f5df0dbbd
saradeepika03@gmail.com <saradeepika03@gmail.com>
2025-08-05 20:57:32 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './index.css';
+
+const App = () => {
+  return (
+    <div style={styles.container}>
+      <h1>Hello GIT!</h1>
+      <p>This is deployed page</p>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '100px',
+    fontFamily: 'Arial, sans-serif',
+  },
+};
+
+export default App;
